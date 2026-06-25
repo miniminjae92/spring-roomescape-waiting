8cee80a6078e626de82cc2e7f06b396173750d54
miniminjae92 <rkdalswoals@gmail.com>
2026-06-26 01:15:29 +0900

    feat(ui): 예약 서비스 화면 구조 개편

--- src/main/java/roomescape/ui/ReservationViewController.java
+++ src/main/java/roomescape/ui/ReservationViewController.java
@@ -8,7 +8,17 @@ public class ReservationViewController {
 
     @GetMapping("/")
     public String home() {
-        return "times";
+        return "index";
+    }
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+
+    @GetMapping("/my-reservation")
+    public String myReservation() {
+        return "my-reservation";
     }
 
     @GetMapping("/login")
@@ -19,5 +29,35 @@ public class ReservationViewController {
     @GetMapping("/admin")
     public String admin() {
         return "admin";
+    }
+
+    @GetMapping("/admin/theme")
+    public String adminTheme() {
+        return "admin-theme";
+    }
+
+    @GetMapping("/admin/date")
+    public String adminDate() {
+        return "admin-date";
+    }
+
+    @GetMapping("/admin/time")
+    public String adminTime() {
+        return "admin-time";
+    }
+
+    @GetMapping("/popular-themes")
+    public String popularThemes() {
+        return "popular-themes";
+    }
+
+    @GetMapping("/admin/slot")
+    public String adminSlot() {
+        return "admin-slot";
+    }
+
+    @GetMapping("/admin/reservation")
+    public String adminReservation() {
+        return "admin-reservation";
     }
 }
